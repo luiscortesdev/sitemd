d1485fc0486beefff64a39f885a368d13ed23b47
luiscortesdev <luiscortesdev@gmail.com>
2026-02-18 15:15:04 -0500

    added nunjucks templating to layouts

--- src/core/buildPage.ts
+++ src/core/buildPage.ts
@@ -1,19 +1,24 @@
 import path from "path"
+import nunjucks from "nunjucks"
 import { readFile } from "fs/promises";
 import type { PageFile } from "../types/PageFile.js";
 import { parsePage } from "./parsePage.js";
+
+const env = nunjucks.configure("layouts", {
+    autoescape: true,
+    noCache: true
+})
 
 export async function buildPage(page: PageFile) {
     const { html, data } = await parsePage(page.absolutePath)
     console.log(data)
 
-    const layoutPath = path.resolve(`layouts/${data.layout}.html`)
-    const layoutTemplate = await readFile(layoutPath, "utf-8")
+    const layout = (data.layout || "default") + ".njk"
 
-    const outputHtml = layoutTemplate
-        .replace("{{ content }}", html)
-        .replace("{{ title }}", data.title || "My SiteMD Page")
-        .replace("{{ description }}", data.description || "My descriptive and well executed SiteMD page")
+    const outputHtml = env.render(layout, {
+        ...data,
+        content: html
+    })
 
     return outputHtml
 }
