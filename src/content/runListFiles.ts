7b2c52cfa54fd5a22a3e49af86060788bac2b9b3
luiscortesdev <luiscortesdev@gmail.com>
2026-03-02 16:51:12 -0500

    fix: changed the scanDir call in runListFiles from resolving to the meta dirname (the dist path) to resolving to the cli process path. This fixed the issue of the listFiles command erroring

--- src/content/runListFiles.ts
+++ src/content/runListFiles.ts
@@ -2,10 +2,12 @@ import path from "path";
 import { scanDir } from "../content/index.js"
 import { loadConfig } from "../core/config/config.js";
 
+const root = process.cwd()
+
 export async function runListFiles() {
     const config = await loadConfig()
     const contentDir = config.contentDir
 
-    let files = await scanDir(path.resolve(import.meta.dirname, contentDir), path.resolve(import.meta.dirname, contentDir))
+    let files = await scanDir(path.resolve(root, contentDir), path.resolve(root, contentDir))
     console.log(files)
 }
