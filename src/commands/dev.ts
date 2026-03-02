8339fb722c1274247caefa0f4d3cbb60252db930
luiscortesdev <luiscortesdev@gmail.com>
2026-03-02 16:37:26 -0500

    refactor: moved all logic related to starting the development server to the separate folder called dev. also created an index file that forwards all of the functions that outside modules need. extracted logic that was in the dev.ts command function into a separate file called runDev.ts. Now, all dev.ts does is call that runDev function.

--- src/commands/dev.ts
+++ src/commands/dev.ts
@@ -1,31 +1,5 @@
-import path from "path"
-import { attachLiveReload } from "../core/attachLiveReload.js"
-import { buildSite } from "../build/index.js"
-import { startServer } from "../core/devServer.js"
-import { watchFiles } from "../core/watchFiles.js"
-import { loadConfig } from "../core/config/config.js"
-import { timer } from "../utils/timer.js"
+import { runDev } from "../dev/index.js"
 
 export async function dev() {
-    const config = await loadConfig()
-    const initialBuildStart = performance.now()
-
-    const root = process.cwd()
-    const outputDir = path.join(root, config.outputDir)
-
-    await buildSite({ dev: true })
-
-    timer("Build", initialBuildStart)
-
-    const server = await startServer(outputDir, config.dev.port)
-    const reload = attachLiveReload(server)
-
-    watchFiles(async () => {
-        const reloadBuildStart = performance.now()
-
-        await buildSite({ dev: true })
-
-        timer("Reload", reloadBuildStart)
-        reload()
-    })
+    await runDev()
 }
---
+++ src/dev/index.ts
@@ -0,0 +1,4 @@
+export { watchFiles } from "./watchFiles.js"
+export { startServer } from "./startServer.js"
+export { attachLiveReload } from "./attachLiveReload.js"
+export { runDev } from "./runDev.js"
