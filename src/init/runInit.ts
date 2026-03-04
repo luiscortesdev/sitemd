b3396e6c9f2d5f5991f596668b961000154126fa
luiscortesdev <luiscortesdev@gmail.com>
2026-03-04 15:18:53 -0500

    fix: fixed runInit to return if every single possible needed file already exists in the user's project

--- src/init/runInit.ts
+++ src/init/runInit.ts
@@ -9,10 +9,10 @@ import { initFolders } from "./initFolders.js"
 export async function runInit(options: { theme: string }) {
     const root = process.cwd()
 
-    //if (fsSync.existsSync(path.join(root, "/layouts")) || fsSync.existsSync(path.join(root, "/public")) || fsSync.existsSync(path.join(root, "/content"))) {
-        //console.log(chalk.yellowBright("🚨 PROJECT IS ALREADY INTIALIZED!"))
-        //return
-    //}
+    if (fsSync.existsSync(path.join(root, "/layouts")) && fsSync.existsSync(path.join(root, "/public")) && fsSync.existsSync(path.join(root, "/content")) && fsSync.existsSync(path.join(root, "/theme")) && fsSync.existsSync(path.join(root, "sitemd.config.js"))) {
+        console.log(chalk.yellowBright("🚨 PROJECT IS ALREADY INTIALIZED!"))
+        return
+    }
 
     console.log(chalk.blue("INITIALIZING PROJECT..."))
     
