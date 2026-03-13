fffd76509dfe6a4e9028d577ea8aaa8752110ef8
luiscortesdev <luiscortesdev@gmail.com>
2026-03-13 12:58:28 -0400

    refactor: made it so every page belongs to the 'all' collection and pages without any collections are added to the 'none' collection. Also changed the all collection to be pushed into the data itself, so it's properly cached

--- src/content/buildCollections.ts
+++ src/content/buildCollections.ts
@@ -3,17 +3,22 @@ import type { CollectionPages } from "./content.types.js"
 
 export function buildCollections(pages: CollectionPages[]): Collections {
     const collections: Collections = {}
+    collections["all"] = []
 
     for (const page of pages) {
         const { data } = page.parsed
         const { route, absolutePath } = page.page
 
         if (!data?.collections) { 
-            data.collections = ["all"]
+            data.collections = ["none"]
         }
+
+        // Add the all tag to every page
+        data.collections.push("all")
 
         const collectionsArray: string[] = data.collections
         
+        // Go through every collection in the page's data and add it to the collections array.
         for (const collectionName of collectionsArray) {
             if (!collections[collectionName]) {
                 collections[collectionName] = []
