c56f9c88f6ad5e096b5c515a076eb62294285546
luiscortesdev <luiscortesdev@gmail.com>
2026-03-18 11:11:43 -0400

    feat: created new invalidateCollections function to properly invalidate layouts whose collection members have changed.

---
+++ src/cache/invalidateCollections.ts
@@ -0,0 +1,6 @@
+import type { SiteMDCache } from "./index.js";
+import type { CollectionPages } from "../collections/index.js";
+
+export function invalidateCollections(cache: SiteMDCache, pages: CollectionPages[]) {
+    
+}
