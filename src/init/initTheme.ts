d38aadf1e4575460a39db90f663ac5f7405aa96a
luiscortesdev <luiscortesdev@gmail.com>
2026-03-22 23:20:05 -0400

    refactor: fixed import statements in outside modules to use bucket index file

--- src/init/initTheme.ts
+++ src/init/initTheme.ts
@@ -4,7 +4,7 @@ import path from "path"
 import chalk from "chalk";
 import { fileURLToPath } from "url";
 
-import { directoryEmpty } from "../utils/fs.js";
+import { directoryEmpty } from "../utils/index.js";
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
