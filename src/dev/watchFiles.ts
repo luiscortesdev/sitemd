6727624cadbec557154e03f4f047d8c59e15c98d
luiscortesdev <luiscortesdev@gmail.com>
2026-03-24 11:17:06 -0400

    feat: added folder that will handle the clean up process after a file is deleted. added an unlink event listener in watchFiles.ts

--- src/dev/watchFiles.ts
+++ src/dev/watchFiles.ts
@@ -1,10 +1,15 @@
 import chokidar from "chokidar"
 
-export function watchFiles(onChange: () => Promise<void>) {
+export function watchFiles(onChange: () => Promise<void>, onDeletion: () => Promise<void>) {
     const watcher = chokidar.watch(
         ["content", "layouts", "public", "theme"],
         { ignoreInitial: true }
     )
+
+    watcher.on("unlink", async () => {
+        console.log("A FILE HAS BEEN DELETED!")
+        await onDeletion()
+    })
 
     watcher.on("all", async () => {
         console.log("FILE CHANGE DETECTED. REBUILDING...")
