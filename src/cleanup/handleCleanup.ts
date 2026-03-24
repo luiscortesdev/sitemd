55aae9ef78a99fb9cde8a4efd3e59f38c5ce84dc
luiscortesdev <luiscortesdev@gmail.com>
2026-03-24 13:29:20 -0400

    feat: implemented src/cleanup/deleteOutput.ts. The function takes the file and deletes it from the output folder. it also deletes the parent folder if it is empty.

--- src/cleanup/handleCleanup.ts
+++ src/cleanup/handleCleanup.ts
@@ -1,6 +1,7 @@
 import PATH from "path"
 import { loadConfig } from "../config/index.js"
 import { deleteCache } from "./deleteCache.js"
+import { deleteOutput } from "./deleteOutput.js"
 
 export async function handleCleanup(path: string) {
     const root = process.cwd()
@@ -17,8 +18,14 @@ export async function handleCleanup(path: string) {
         console.log(`${fullPath} IS NOT A VALID PATH!`)
         return
     }
+    
+    const topAndSecondLevelDir = PATH.join(topLevelDir, secondLevelDir)
 
-    if (topLevelDir === config.contentDir || topLevelDir === config.layoutsDir || PATH.join(topLevelDir, secondLevelDir) === PATH.join(config.themeDir, config.layoutsDir)) {
+    if (topLevelDir === config.contentDir || topLevelDir === config.layoutsDir || topAndSecondLevelDir === PATH.join(config.themeDir, config.layoutsDir)) {
         deleteCache(fullPath, fileName, topLevelDir, secondLevelDir)
+    }
+
+    if (topLevelDir === config.contentDir || topLevelDir === config.publicDir || topAndSecondLevelDir === PATH.join(config.themeDir, config.publicDir)) {
+        deleteOutput(fullPath, path)
     }
 }
