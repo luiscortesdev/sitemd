59a472fa6ec10fccb4074df29bf9141898a9ca2f
luiscortesdev <luiscortesdev@gmail.com>
2026-03-31 17:35:02 -0400

    refactor: updated the descriptions for commands in index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { listfiles, build, dev, init } from "./commands/index.js"
 
 program
     .command("listfiles")
-    .description("List all of the current files in the content directory")
+    .description("List all of the current files in the content directory.")
     .action(listfiles)
 
 program
@@ -15,12 +15,12 @@ program
 
 program
     .command("build")
-    .description("Parses your markdown files in content into html")
+    .description("Builds your project into a ready to deploy static site.")
     .action(build)
 
 program
     .command("dev")
-    .description("Starts up a development server to see your changes in real-time")
+    .description("Starts up a development server to see your changes in real-time.")
     .action(dev)
 
 program
