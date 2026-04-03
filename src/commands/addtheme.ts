2c8764d436676ad215812306ae6ea8e410a52aab
luiscortesdev <luiscortesdev@gmail.com>
2026-04-03 17:19:23 -0400

    feat: complete addtheme handler in commands folder

--- src/commands/addtheme.ts
+++ src/commands/addtheme.ts
@@ -1,3 +1,5 @@
+import { runAddTheme } from "../theme/runAddTheme.js"
+
 export async function addtheme(options: { theme: string }) {
-    
+    await runAddTheme(options)
 }
---
+++ src/theme/runAddTheme.ts
@@ -0,0 +1,5 @@
+import inquirer from "inquirer";
+
+export async function runAddTheme(options: { theme: string }) {
+    
+}
