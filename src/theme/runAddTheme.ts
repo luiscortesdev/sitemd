572b102ec46727bec803900926276bbfc401084e
luiscortesdev <luiscortesdev@gmail.com>
2026-04-03 21:55:05 -0400

    feat: added inquirer confirm message to addTheme for theme replacements

--- src/theme/runAddTheme.ts
+++ src/theme/runAddTheme.ts
@@ -4,18 +4,32 @@ import chalk from "chalk";
 import inquirer from "inquirer";
 
 import { fileURLToPath } from "url";
+import { loadConfig } from "../config/index.js";
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export async function runAddTheme(options: { theme: string }) {
     const root = process.cwd()
+    const config = await loadConfig()
     const requestedThemePath = path.join(__dirname, `../templates/themes/${options.theme}`)
 
     try {
         await fs.access(requestedThemePath)
         
     } catch {
-        console.log(chalk.redBright(`❌ COULD NOT FIND ${options.theme} THEME. ENSURE IT EXISTS!r5fr`))
+        console.log(chalk.redBright(`❌ COULD NOT FIND ${options.theme} THEME. ENSURE IT EXISTS!`))
     }
+
+    inquirer.prompt(
+        [
+            {
+                type: "confirm",
+                name: "rewriteTheme",
+                message: `ARE YOU SURE YOU WANT TO REPLACE YOU CURRENT THEME ${config.theme} WITH ${options.theme}? `,
+            }
+        ],
+    ).then((answer) => {
+        console.log(answer.rewriteTheme)
+    })
 }
