409872301c3173133b2f1fc6302940759afaf9cd
luiscortesdev <luiscortesdev@gmail.com>
2026-04-03 21:47:10 -0400

    feat: added theme exist check to runAddTheme.ts

--- src/theme/runAddTheme.ts
+++ src/theme/runAddTheme.ts
@@ -1,5 +1,21 @@
+import fs from "fs/promises"
+import path from "path";
+import chalk from "chalk";
 import inquirer from "inquirer";
 
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export async function runAddTheme(options: { theme: string }) {
-    
+    const root = process.cwd()
+    const requestedThemePath = path.join(__dirname, `../templates/themes/${options.theme}`)
+
+    try {
+        await fs.access(requestedThemePath)
+        
+    } catch {
+        console.log(chalk.redBright(`❌ COULD NOT FIND ${options.theme} THEME. ENSURE IT EXISTS!r5fr`))
+    }
 }
