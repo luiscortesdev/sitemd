2302779d8ad4921119d6bd8bcc46fe50cbe54ac2
luiscortesdev <luiscortesdev@gmail.com>
2026-04-13 14:08:34 -0400

    feat: added cache utilization to src/collections/buildCollections.ts.

--- src/collections/buildCollections.ts
+++ src/collections/buildCollections.ts
@@ -1,4 +1,4 @@
-import { loadCache } from "../cache/cache.js"
+import { loadCache } from "../cache/index.js"
 import type { Collections, CollectionPages } from "./collections.types.js"
 
 export async function buildCollections(pages: CollectionPages[]): Promise<Collections> {
@@ -11,32 +11,52 @@ export async function buildCollections(pages: CollectionPages[]): Promise<Collec
         const { route, absolutePath } = page.page
 
         const cached = cache.pages[absolutePath]
+        
+        // If the page is cached, the cached hash matches the current hash, and it has an array of collections cached, then we can skip parsing its
+        // collections.
+        if (cached && cached.hash === page.hash && cached.parsed?.data.collections) {
+            const collectionsArray = cached.parsed.data.collections
+            const { data } = cached.parsed
 
-        const { data } = page.parsed
+            for (const collectionName of collectionsArray) {
+
+                if (!collections[collectionName]) {
+                    collections[collectionName] = []
+                }
+
+                collections[collectionName].push({
+                    ...data,
+                    url: route,
+                    path: absolutePath
+                })
+            }
+        } else {
+            const { data } = page.parsed
         
 
-        if (!data?.collections) { 
-            data.collections = ["none"]
-        }
-
-        // Add the all tag to every page if it does not already have it.
-        if (!data.collections.includes("all")) {
-            data.collections.push("all")
-        }
-
-        const collectionsArray: string[] = data.collections
-        
-        // Go through every collection in the page's data and add it to the collections array.
-        for (const collectionName of collectionsArray) {
-            if (!collections[collectionName]) {
-                collections[collectionName] = []
+            if (!data?.collections) { 
+                data.collections = ["none"]
             }
 
-            collections[collectionName].push({
-                ...data,
-                url: route,
-                path: absolutePath
-            })
+            // Add the all tag to every page if it does not already have it.
+            if (!data.collections.includes("all")) {
+                data.collections.push("all")
+            }
+
+            const collectionsArray: string[] = data.collections
+            
+            // Go through every collection in the page's data and add it to the collections array.
+            for (const collectionName of collectionsArray) {
+                if (!collections[collectionName]) {
+                    collections[collectionName] = []
+                }
+
+                collections[collectionName].push({
+                    ...data,
+                    url: route,
+                    path: absolutePath
+                })
+            }
         }
 
     }
