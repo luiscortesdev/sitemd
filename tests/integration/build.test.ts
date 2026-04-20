e826c190b471ed7fe75f3e981075d8ba757b22f1
luiscortesdev <luiscortesdev@gmail.com>
2026-04-20 14:00:06 -0400

    feat: added basic file creation tests to src/build/build.test.ts. updated .gitignore to ignore output folders from the test runs.

--- tests/integration/build.test.ts
+++ tests/integration/build.test.ts
@@ -1,12 +1,13 @@
-import { describe, it, expect, vi, beforeAll, afterAll, afterEach } from "vitest"
+import { describe, it, expect, vi, beforeAll, afterAll } from "vitest"
 import path from "path"
 import fs from "fs-extra"
 import { build } from "../../src/commands/build"
 
 describe("SiteMD Build Pipeline", () => {
     const fixturePath = path.resolve(__dirname, "../fixtures/basic-site")
-    const outDir = path.join(fixturePath, "dist")
+    const outDir = path.join(fixturePath, "_site")
 
+    // Configure the framework to function properly with tests.
     beforeAll(async () => {
         vi.spyOn(process, "cwd").mockReturnValue(fixturePath)
 
@@ -18,10 +19,34 @@ describe("SiteMD Build Pipeline", () => {
 
         await build()
     })
-
     afterAll(() => {
         vi.restoreAllMocks()
     })
 
-    it("Should generate an index.html file")
+    // Basic file creation tests.
+    it("Should generate an index.html file", () => {
+        const indexPath = path.join(outDir, "index.html")
+
+        expect(fs.existsSync(indexPath)).toBe(true)
+    })
+    it("Should generate an about/index.html file", () => {
+        const aboutPath = path.join(outDir, "about", "index.html")
+
+        expect(fs.existsSync(aboutPath)).toBe(true)
+    })
+    it("Should generate a blog/index.html file", () => {
+        const blogPath = path.join(outDir, "blog", "index.html")
+
+        expect(fs.existsSync(blogPath)).toBe(true)
+    })
+    it("Should generate a blog/posts/post1/index.html file", () => {
+        const post1Path = path.join(outDir, "blog", "posts", "post1", "index.html")
+
+        expect(fs.existsSync(post1Path)).toBe(true)
+    })
+    it("Should generate a blog/posts/post2/index.html file", () => {
+        const post2Path = path.join(outDir, "blog", "posts", "post2", "index.html")
+
+        expect(fs.existsSync(post2Path)).toBe(true)
+    })
 })
