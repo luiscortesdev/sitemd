0075b33e6c9992b8f3953404f175966fc84a700d
luiscortesdev <luiscortesdev@gmail.com>
2026-04-20 12:39:11 -0400

    feat: added unit test for src/utils/comparisons.ts function areStringArraysEqual.

---
+++ tests/unit/utils.test.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect } from "vitest";
+import { areStringArraysEqual } from "../../src/utils/index"
+
+describe("Utility Functions", () => {
+    it("Should correctly compare two identical arrays", () => {
+        const arr1 = ["content\\blog\\index.md", "content\\index.md", "content\\blog\\posts\\index.md"]
+        const arr2 = ["content\\blog\\index.md", "content\\index.md", "content\\blog\\posts\\index.md"]
+
+        expect(areStringArraysEqual(arr1, arr2)).toBe(true)
+    })
+
+    it("Should correctly detect when the arrays are different, but have the same length", () => {
+        const arr1 = ["content\\blog\\index.md", "content\\index.md", "content\\blog\\post\\index.md"]
+        const arr2 = ["content\\blog\\index.md", "content\\index.md", "content\\blog\\posts\\index.md"]
+
+        expect(areStringArraysEqual(arr1, arr2)).toBe(false)
+    })
+
+    it("Should correctly detect when the arrays are different and have different lengths", () => {
+        const arr1 = ["content\\blog\\index.md", "content\\index.md", "content\\blog\\posts\\index.md"]
+        const arr2 = ["content\\blog\\index.md", "content\\index.md", "content\\blog\\posts\\index.md", "content\\about\\index.md", "content\\blog\\posts\\1\\index.md"]
+
+        expect(areStringArraysEqual(arr1, arr2)).toBe(false)
+    })
+})
