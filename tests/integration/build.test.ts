d6be1704cddf75c8eeac54b1435ab099778cca4a
luiscortesdev <luiscortesdev@gmail.com>
2026-04-20 15:26:26 -0400

    feat: added nunjucks layout testing and applying custom attributes testing to the integration tests.

--- tests/integration/build.test.ts
+++ tests/integration/build.test.ts
@@ -1,4 +1,5 @@
 import { describe, it, expect, vi, beforeAll, afterAll } from "vitest"
+import { JSDOM } from "jsdom"
 import path from "path"
 import fs from "fs-extra"
 import { build } from "../../src/commands/build"
@@ -6,6 +7,17 @@ import { build } from "../../src/commands/build"
 describe("SiteMD Build Pipeline", () => {
     const fixturePath = path.resolve(__dirname, "../fixtures/basic-site")
     const outDir = path.join(fixturePath, "_site")
+
+    // Paths to Expected Html Files
+    const indexPath = path.join(outDir, "index.html")
+    const aboutPath = path.join(outDir, "about", "index.html")
+    const blogPath = path.join(outDir, "blog", "index.html")
+    const post1Path = path.join(outDir, "blog", "posts", "post1", "index.html")
+    const post2Path = path.join(outDir, "blog", "posts", "post2", "index.html")
+
+    // Placeholder for document object for html files we will test.
+    let homeDocument: Document;
+    let blogDocument: Document;
 
     // Configure the framework to function properly with tests.
     beforeAll(async () => {
@@ -18,17 +30,16 @@ describe("SiteMD Build Pipeline", () => {
         }
 
         await build()
+
+        const homeDom = new JSDOM(fs.readFileSync(indexPath, "utf-8"))
+        const blogDom = new JSDOM(fs.readFileSync(blogPath, "utf-8"))
+
+        homeDocument = homeDom.window.document
+        blogDocument = blogDom.window.document
     })
     afterAll(() => {
         vi.restoreAllMocks()
     })
-
-    // Paths to Expected Html Files
-    const indexPath = path.join(outDir, "index.html")
-    const aboutPath = path.join(outDir, "about", "index.html")
-    const blogPath = path.join(outDir, "blog", "index.html")
-    const post1Path = path.join(outDir, "blog", "posts", "post1", "index.html")
-    const post2Path = path.join(outDir, "blog", "posts", "post2", "index.html")
 
     // Basic file creation tests.
     it("Should generate an index.html file", () => {
@@ -49,10 +60,52 @@ describe("SiteMD Build Pipeline", () => {
 
     // Custom Attributes Tests
     it("Should apply custom attributes", () => {
-        const homeHtml = fs.readFileSync(indexPath, "utf-8")
-        expect(homeHtml).toContain(`<h1 data-aria="home" id="home" class="text-green">Hello Vitest</h1>`)
+        const mainHeading = homeDocument.querySelector(".text-green")
+        expect(mainHeading).not.toBeNull()
+        expect(mainHeading?.getAttribute("data-aria")).toBe("home")
+        const mainHeadingWithId = homeDocument.getElementById("home")
+        expect(mainHeadingWithId).not.toBeNull()
 
-        const blogHtml = fs.readFileSync(blogPath, "utf-8")
-        expect(blogHtml).toContain(`<h2 class="text-green">Welcome to my test blog.</h2>`)
+        
+        const blogHeading = blogDocument.querySelector(".text-green")
+        expect(blogHeading).not.toBeNull()
+    })
+
+    // Nunjucks Tests
+    it("Should wrap content in nunjucks layout", () => {
+        const mainTag = homeDocument.querySelector("main")
+        expect(mainTag).not.toBeNull()
+    })
+
+    const posts = [
+        {
+            href: "/blog/posts/post1",
+            innerText: "Post 1 : The First Post"
+        },
+        {
+            href: "/blog/posts/post2",
+            innerText: "Post 2 : The Second Post"
+        },
+    ]
+    it("Should create a list of posts from 'posts' collection", () => {
+        const outerUlTag = blogDocument.getElementById("posts")
+        expect(outerUlTag).not.toBeNull()
+
+        if (!outerUlTag) return
+
+        console.log(outerUlTag.innerHTML)
+
+        Array.from(outerUlTag.children).forEach((child: any, index) => {
+            expect(child.tagName).toBe("LI")
+
+            const childATag = child.children[0]
+            const childATagInfo = posts[index]
+            expect(childATag.tagName).toBe("A")
+            expect(childATag.innerHTML).toBe(childATagInfo.innerText)
+            expect(childATag.href).toBe(childATagInfo.href)
+            
+        })
+
+        
     })
 })
