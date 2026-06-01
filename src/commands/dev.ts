96958c4a55264905a3d6d564fa2b5f7987823179
luiscortesdev <luiscortesdev@gmail.com>
2026-06-01 18:19:27 -0400

    ui: added building message when the dev server is initially spun up.

--- src/commands/dev.ts
+++ src/commands/dev.ts
@@ -1,10 +1,11 @@
 import { runDev } from "../dev/index.js"
-import { setDebugMode } from "../utils/index.js"
+import { logger, setDebugMode } from "../utils/index.js"
 
 export async function dev(options: { debug: boolean }) {
     if (options.debug) {
         setDebugMode(true)
     }
 
+    logger.process("BUILDING SITE...")
     await runDev()
 }
