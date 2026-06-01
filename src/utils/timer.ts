fbcf8392744425e3602aa100d9826cbb64915350
luiscortesdev <luiscortesdev@gmail.com>
2026-05-31 20:19:50 -0400

    ui: updated log messages in timer util to be all caps, conforming with the overall framework typography.

--- src/utils/timer.ts
+++ src/utils/timer.ts
@@ -6,14 +6,14 @@ export function timer(label: TimerLabels, start: number) {
     const ms = performance.now() - start
 
     if (label === "Build") {
-        logger.success(`Built project in ${ms.toFixed(1)}ms`)
+        logger.success(`BUILT PROJECT IN ${ms.toFixed(1)}ms`)
     }
 
     if (label === "Reload") {
-        logger.success(`Rebuilt project in ${ms.toFixed(1)}ms`)
+        logger.success(`REBUILT PROJECT IN ${ms.toFixed(1)}ms`)
     }
 
     if (label === "Cleanup") {
-        logger.process(`Cleaned up file in ${ms.toFixed(1)}ms`)
+        logger.process(`CLEANED UP FILE IN ${ms.toFixed(1)}ms`)
     }
 }
