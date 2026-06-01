6f0d84d991f417ea6c284ffb6b23f8f68c71b55b
luiscortesdev <luiscortesdev@gmail.com>
2026-06-01 18:00:57 -0400

    feat: added proper error handling to the build command.

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -11,7 +11,17 @@ export async function build(options: { debug: boolean }) {
     logger.process("BUILDING SITE...")
     const initialBuildStart = performance.now()
 
-    await buildSite({ dev: false })
+    try {
+        await buildSite({ dev: false })
+    } catch (error) {
+        if (error instanceof Error) {
+            logger.error(`BUILD PROCESS FAILED: ${error.message}`)
+        } else {
+            logger.error("BUILD PROCESS FAILED DUE TO AN UNKNOWN ERROR.")
+        }
+
+        process.exit(1)
+    }
 
     timer("Build", initialBuildStart)
 }
