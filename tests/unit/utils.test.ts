33248aa0abdc6ec10c2d6b5410bd8dff8d300887
luiscortesdev <luiscortesdev@gmail.com>
2026-06-08 21:51:44 -0400

    test: added various tests for normalizePath util function.

--- tests/unit/utils.test.ts
+++ tests/unit/utils.test.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect } from "vitest";
 
 import { areStringArraysEqual } from "../../src/utils/index"
+import { normalizePath } from "../../src/utils/index";
 
 describe("Utility Functions", () => {
     it("Should correctly compare two identical arrays", () => {
@@ -22,5 +23,19 @@ describe("Utility Functions", () => {
         const arr2 = ["content\\blog\\index.md", "content\\index.md", "content\\blog\\posts\\index.md", "content\\about\\index.md", "content\\blog\\posts\\1\\index.md"]
 
         expect(areStringArraysEqual(arr1, arr2)).toBe(false)
+    })
+
+    it("Should properly normalize windows os paths with forward slashes", () => {
+        const windowsPath = "C:\\Users\\Username\\Documents\\SiteMDProject\\layouts\\default.njk"
+        const expectedPath = "C:/Users/Username/Documents/SiteMDProject/layouts/default.njk"
+
+        expect(normalizePath(windowsPath, "\\")).toBe(expectedPath)
+    })
+
+    it("Should properly normalize linux/mac os paths", () => {
+        const linuxPath = "/Users/Username/Documents/SiteMDProject/layouts/default.njk"
+        const expectedPath = "/Users/Username/Documents/SiteMDProject/layouts/default.njk"
+
+        expect(normalizePath(linuxPath, "/")).toBe(expectedPath)
     })
 })
