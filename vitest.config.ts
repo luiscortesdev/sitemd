ed374ca4316c6f921d5f7839c87dbf1e5bcde70e
luiscortesdev <luiscortesdev@gmail.com>
2026-06-17 11:39:44 -0400

    chore (vitest): update config to include DEBUG env variable for testing debug logs.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
     test: {
         environment: "node",
+        env: {
+            DEBUG: process.env.DEBUG || "false"
+        },
         include: ["tests/**/*.test.ts"],
     },
     server: {
